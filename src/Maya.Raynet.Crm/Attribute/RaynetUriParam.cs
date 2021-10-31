d7ed7d13b21dfeb333b9fcb1ac30a3cec0aca869
Salim Mayaleh <salim.mayaleh@gmail.com>
2021-10-31 09:55:03 +0100

    add custom attribute

---
+++ src/Maya.Raynet.Crm/Attribute/RaynetUriParam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maya.Raynet.Crm.Attribute
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false)]
+    public class RaynetUriParam : System.Attribute
+    {
+        private readonly string name;
+
+        public RaynetUriParam(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual string Name
+        {
+            get { return this.name; }
+        }
+    }
+}
